7625184646fbdefe6ff25fb5b46bccbf808f64d0
Tibor22 <tothtibitravel@gmail.com>
2022-02-28 17:39:38 +0000

    responsive-nav

---
+++ app.js
@@ -0,0 +1,15 @@
+// const navLinks = document.querySelectorAll(".nav li a");
+
+// window.addEventListener("resize", () => {
+//   const screenSize = document.body.clientWidth;
+//   console.log(screenSize);
+//   if (screenSize < 900) {
+//     navLinks.forEach((link) => {
+//       //   link.text = "";
+//       console.log(link);
+//     });
+//     console.log(navLinks);
+//   }
+// });
+
+// console.log(navLinks);
