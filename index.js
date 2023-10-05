7bddb53af6ee97e625d829a923383854c4e83361
Tayokanch <tayoquadri11@gmail.com>
2023-10-05 18:24:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const body = document.body
+const aside = document.querySelector('aside')
+const logo = document.querySelector('#logo')
+const icons = document.querySelectorAll('.icon')
+const playlist = document.querySelector('#playlist')
+const sideBar = document.querySelector('#side-bar')
+const playListName = document.querySelector('.playlist-name')
+const playlistH3 = playlist.querySelector('h3')
+const bar = document.querySelector('#bar')
+const logoSvg = logo.querySelector('svg')
+const createPlaylist = document.querySelector('.create-playlist');
+
+
+
+
+/* Nav Bar */
+let closeBar = true;
+bar.addEventListener('click', function() {
+    if (closeBar) {
+        body.style.gridTemplateColumns = '70px 1fr';
+        logoSvg.style.display = 'none'
+        playlistH3.style.display ='none'
+        playListName.style.display ='none'
+        createPlaylist.style.display ='none'
+
+
+        icons.forEach((icon) =>{
+        const iconPH3 = icon.querySelector(':nth-child(2n)')
+        iconPH3.style.display ='none'
+        })  
+   
+        closeBar = false;
+    } 
+
+    else if (window.innerWidth > 600) {
+        body.style.gridTemplateColumns = '200px 1fr';
+        logoSvg.style.display = ''
+        playlistH3.style.display =''
+        playListName.style.display =''
+        createPlaylist.style.display =''
+
+   
+        icons.forEach((icon) =>{
+            const iconPH3 = icon.querySelector(':nth-child(2n)')
+            iconPH3.style.display =''
+         
+        })  
+        
+   
+        closeBar = true;
+      
+    }
+    
+});
